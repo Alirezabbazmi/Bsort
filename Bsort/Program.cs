f53e6e1e59bdeb998368f551284f125360f8c6a5
Alireza <Alireza@DESKTOP-K5M99NB>
2021-12-13 09:55:19 -0800

    Add project files.

---
+++ Bsort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // درست کردن نمونه 
+            var sort = new Sorting();
+            //دادن یک ارایه به متد و بازگشت ارایه سورت شده
+            var sortedarry = sort.bubbleSort(new int[] { 1, 5, 10, 12, 3 ,468,54,22});
+            Console.WriteLine("\n");
+            //چاپ ارایه سرت شده
+            foreach (var item in sortedarry)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Bsort/Sorting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bsort
+{
+    class Sorting
+    {
+        //بابل سرت :یک ارایه را میگیرد و یه ارایه سرت شده برمیگرداند
+        public int[] bubbleSort( int[] arry )
+        {
+            //طول ارایه
+            var lenght = arry.Length;
+            //ساختار الگوریتم
+            for (int i = 0; i < lenght-1; i++)
+            {
+                for (int j = 0; j < lenght - i - 1; j++)
+                {
+                    //اگر دیتای اندیس اوا بزگ تر از اندیس بعدی بود
+                    if (arry[j] > arry[j + 1])
+                    {
+                        //تعویض اندیس ها
+                        var samp = arry[j];
+                        arry[j] = arry[j + 1];
+                        arry[j + 1] = samp;
+                        Console.WriteLine($"we swap space "+j+" and "+(j+1)+" :  values  was vlaue "+arry[j]+" and " + arry[j+1] );
+                    }
+                }
+            }
+            //برگرداندن ارایه سرت شده
+            return arry;
+        }
+    }
+}
